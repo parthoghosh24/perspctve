46080f68db07a02554f736537a0d7a52b5350bba
Partho Ghosh <parthoghosh@Parthos-MacBook-Pro.local>
2021-07-09 20:23:05 +0530

    Working on the homepage

--- perspctve_fe/src/App.js
+++ perspctve_fe/src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-500">Hello Tailwind</h1>
+      <header>
+
+      </header>
+      <div className="absolute inset-0 flex items-center justify-center mx-auto">
+          <h1 className="font-serif text-7xl text-white">Perspctve</h1>
+          <h3 className="font-serif text-4xl text-white">
+            <p>"Because you have a perspective.</p>
+            <p>Share it and find out what the world thinks."</p>
+          </h3>
+      </div>
     </div>
   );
 }
