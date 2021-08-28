05b5a7fe3c2555001099d8a3a8f7ca705563098b
parthoghosh24 <partho.ghosh24@gmail.com>
2021-08-28 19:22:32 +0530

    deploy.rb updated to use sudo

--- perspctve_api/config/deploy.rb
+++ perspctve_api/config/deploy.rb
@@ -17,7 +17,7 @@ set :assets_roles, []
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
+set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}/perspctve_api"
