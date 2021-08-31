c5547c5dbd15c6cd4082682e85c16b4435b92e06
parthoghosh24 <partho.ghosh24@gmail.com>
2021-08-31 19:23:12 +0530

    Updated stupid bug

--- perspctve_api/app/services/opinion_service.rb
+++ perspctve_api/app/services/opinion_service.rb
@@ -9,7 +9,7 @@ class OpinionService
       tags = @opinion_params.delete(:tags)
       @opinion_params.delete(:media) if @opinion_params[:media] && !@opinion_params[:media].key?(:type)
       @opinion_params[:user] = @author
-      @opinion_params[:is_anonymous] = opinion_params.include?(:is_anonymous) ? opinion_params[:is_anonymous] : false
+      @opinion_params[:is_anonymous] = @opinion_params.include?(:is_anonymous) ? @opinion_params[:is_anonymous] : false
       @opinion_params[:uuid] = SecureRandom.uuid
       opinion = Opinion.new(@opinion_params)
       if (!opinion.save)
