d98baa3c8c79c960a4f136ddaa60f180605b7faf
parthoghosh24 <partho.ghosh24@gmail.com>
2021-09-01 18:45:22 +0530

    Added float to button

--- perspctve_fe/src/components/common/ButtonLoading.jsx
+++ perspctve_fe/src/components/common/ButtonLoading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtonLoading = () => {
   return (
-    <div className ="flex space-x-2 p-5 justify-center items-center">
+    <div className ="flex space-x-2 p-5 justify-center items-center float-left">
       <div
           className="bg-blue-800 p-2  w-4 h-4 rounded-full animate-bounce white-circle"
         ></div>
